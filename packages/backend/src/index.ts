296fe6f586d566cf9eca209b363429ab3cdb0b29
neeraj <neeraj.rtly@gmail.com>
2024-01-15 10:32:43 +0530

    Setup database connection

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,6 +1,23 @@
 import express, { Request, Response } from 'express';
 
 // TODO: Add db connection setup here
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import { User } from '../entity/User';
+
+const AppDataSource = new DataSource({
+  type: 'better-sqlite3',
+  database: 'db/Snowball',
+  entities: [User],
+  synchronize: true,
+  logging: false
+});
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log('database initialized');
+  })
+  .catch((error) => console.log(error));
 
 const app = express();
 const port = 8080;
