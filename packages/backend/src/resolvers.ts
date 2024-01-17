a2f61303d70b46f2b79e17312698d686a22eedf5
prathamesh0 <42446521+prathamesh0@users.noreply.github.com>
2024-01-17 16:59:59 +0530

    Implement GQL resolver to get organizations data (#20)
    * Add resolver method for getting organizations
    
    * Fetch project data along with organizations from db
    
    * Refactor code to map db project entity to graphql type
    
    ---------
    
    Co-authored-by: neeraj <neeraj.rtly@gmail.com>

--- packages/backend/src/resolvers.ts
+++ packages/backend/src/resolvers.ts
@@ -1,15 +1,34 @@
 import { Database } from './database';
+import { projectToGqlType } from './utils';
 
 export const createResolvers = async (db: Database): Promise<any> => {
   return {
     Query: {
-      user: (
-        _: any,
-        __: any,
-        // TODO: add custom type for context
-        context: any
-      ) => {
+      // TODO: add custom type for context
+      user: (_: any, __: any, context: any) => {
         return db.getUser(context.userId);
+      },
+
+      organizations: async (_:any, __: any, context: any) => {
+        const organizations = await db.getOrganizationsbyUserId(context.userId);
+
+        const orgsWithProjectsPromises = organizations.map(async (org) => {
+          const dbProjects = await db.getProjectsbyOrganizationId(org.id);
+
+          const projects = dbProjects.map(dbProject => {
+            return projectToGqlType(dbProject);
+          });
+
+          return {
+            ...org,
+            projects
+          };
+        });
+
+        const orgsWithProjects = await Promise.all(orgsWithProjectsPromises);
+
+        // TODO: Populate members field when / if required
+        return orgsWithProjects;
       }
     }
   };
