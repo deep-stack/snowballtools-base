239ff1ec95c7f464361b061f329f9a35cbcdc037
neeraj <neeraj.rtly@gmail.com>
2024-01-17 16:34:22 +0530

    Integrate gql-client in frontend app

---
+++ packages/frontend/src/context/GQLClientContext.tsx
@@ -0,0 +1,24 @@
+import React, { createContext, useContext, ReactNode } from 'react';
+import { GQLClient } from 'gql-client';
+
+const GQLClientContext = createContext({});
+
+export const GQLClientProvider = ({
+  client,
+  children,
+}: {
+  children: ReactNode;
+  client: GQLClient;
+}) => (
+  <GQLClientContext.Provider value={client}>
+    {children}
+  </GQLClientContext.Provider>
+);
+
+export const useGQLClient = () => {
+  const client = useContext(GQLClientContext);
+  if (!client) {
+    throw new Error('useGQLClient must be used within a GQLClientProvider');
+  }
+  return client;
+};
