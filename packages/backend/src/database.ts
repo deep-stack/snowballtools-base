e4c099f8c368e814ea1d66fbfd5f0068e320fe7f
neerajvijay1997 <111040298+neerajvijay1997@users.noreply.github.com>
2024-01-17 10:53:01 +0530

    Refactor database setup and add user context to GQL requests (#17)
    * Add context to each request
    
    * Implement get user db query
    
    * Move constants to a separate file
    
    * Refactor database init method

--- packages/backend/src/database.ts
+++ packages/backend/src/database.ts
@@ -2,23 +2,35 @@ import { DataSource } from 'typeorm';
 import path from 'path';
 import debug from 'debug';
 
-const log = debug('snowball:server');
+import { User } from './entity/User';
+import { DatabaseConfig } from './config';
 
-export const initializeDatabase = async (
-  database: string = 'db/snowball'
-): Promise<void> => {
-  try {
-    const AppDataSource = new DataSource({
+const log = debug('snowball:database');
+
+export class Database {
+  private dataSource: DataSource;
+
+  constructor ({ dbPath }: DatabaseConfig) {
+    this.dataSource = new DataSource({
       type: 'better-sqlite3',
-      database,
+      database: dbPath,
       entities: [path.join(__dirname, '/entity/*')],
       synchronize: true,
       logging: false
     });
-
-    await AppDataSource.initialize();
-    log('database initialized');
-  } catch (error) {
-    log(error);
   }
-};
+
+  async init () : Promise<void> {
+    await this.dataSource.initialize();
+    log('database initialized');
+  }
+
+  async getUser (userId: number) : Promise<User | null> {
+    const userRepository = this.dataSource.getRepository(User);
+    const user = await userRepository.findOneBy({
+      id: userId
+    });
+
+    return user;
+  }
+}
