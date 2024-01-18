70e5cac2ffabe436d2851646d60c1921fdeea8b4
prathamesh0 <42446521+prathamesh0@users.noreply.github.com>
2024-01-18 10:04:02 +0530

    Integrate gql-client in frontend (#21)
    * Integrate gql-client in frontend app
    
    * Populate home page information using gql-client
    
    * Remove non required fields from organizations query
    
    ---------
    
    Co-authored-by: neeraj <neeraj.rtly@gmail.com>

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -1,21 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import assert from 'assert';
+import { Toaster } from 'react-hot-toast';
+import { GQLClient } from 'gql-client';
 
 import { ThemeProvider } from '@material-tailwind/react';
 
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Toaster } from 'react-hot-toast';
+import { GQLClientProvider } from './context/GQLClientContext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
+
+const gqlEndpoint = process.env.REACT_APP_GQL_SERVER_URL;
+assert(gqlEndpoint, 'GQL server URL not provided');
+
+const gqlClient = new GQLClient({ gqlEndpoint });
+
 root.render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
-      <Toaster position="bottom-center" />
+      <GQLClientProvider client={gqlClient}>
+        <App />
+        <Toaster position="bottom-center" />
+      </GQLClientProvider>
     </ThemeProvider>
   </React.StrictMode>,
 );
