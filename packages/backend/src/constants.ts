defee719b247a3811777323e2abe16daf815470c
neeraj <neeraj.rtly@gmail.com>
2024-01-19 15:09:15 +0530

    Rename files in gql-client package

--- packages/backend/src/constants.ts
+++ packages/backend/src/constants.ts
@@ -1,3 +1,5 @@
+export const DEFAULT_CONFIG_FILE_PATH = 'environments/local.toml';
+
 export const DEFAULT_GQL_PATH = '/graphql';
 
 // Note: temporary hardcoded user, later to be derived from auth token
