31f4ccae1b019c7780395f8fa70e26ef6f3d74c0
neeraj <neeraj.rtly@gmail.com>
2024-01-23 19:29:41 +0530

    Populate userOrganization entity with test data

--- packages/backend/test/initialize-db.ts
+++ packages/backend/test/initialize-db.ts
@@ -1,6 +1,7 @@
 import { DataSource, DeepPartial, EntityTarget, ObjectLiteral } from 'typeorm';
 import * as fs from 'fs/promises';
 import debug from 'debug';
+import path from 'path';
 
 import { User } from '../src/entity/User';
 import { Organization } from '../src/entity/Organization';
@@ -12,6 +13,11 @@ import { ProjectMember } from '../src/entity/ProjectMember';
 import { Deployment } from '../src/entity/Deployment';
 
 const log = debug('snowball:initialize-database');
+
+const USER_DATA_PATH = './fixtures/users.json';
+const PROJECT_DATA_PATH = './fixtures/projects.json';
+const ORGANIZATION_DATA_PATH = './fixtures/organizations.json';
+const USER_ORGANIZATION_DATA_PATH = './fixtures/user-orgnizations.json';
 
 const loadAndSaveData = async <Entity extends ObjectLiteral>(entityType: EntityTarget<Entity>, dataSource: DataSource, filePath: string) => {
   const entitiesData = await fs.readFile(filePath, 'utf-8');
@@ -29,11 +35,11 @@ const loadAndSaveData = async <Entity extends ObjectLiteral>(entityType: EntityT
   return savedEntity;
 };
 
-const loadAndSaveProjectsData = async (dataSource: DataSource, filePath: string) => {
-  const savedUsers = await loadAndSaveData(User, dataSource, 'test/fixtures/users.json');
-  const savedOrgs = await loadAndSaveData(Organization, dataSource, 'test/fixtures/organizations.json');
+const generateTestData = async (dataSource: DataSource) => {
+  const savedUsers = await loadAndSaveData(User, dataSource, path.resolve(__dirname, USER_DATA_PATH));
+  const savedOrgs = await loadAndSaveData(Organization, dataSource, path.resolve(__dirname, ORGANIZATION_DATA_PATH));
 
-  const projectsData = await fs.readFile(filePath, 'utf-8');
+  const projectsData = await fs.readFile(path.resolve(__dirname, PROJECT_DATA_PATH), 'utf-8');
   const projects = JSON.parse(projectsData);
   const projectRepository = dataSource.getRepository(Project);
 
@@ -42,6 +48,18 @@ const loadAndSaveProjectsData = async (dataSource: DataSource, filePath: string)
     project.owner = savedUsers[projectData.ownerIndex];
     project.organization = savedOrgs[projectData.organizationIndex];
     await projectRepository.save(project);
+  }
+
+  const userOrgData = await fs.readFile(path.resolve(__dirname, USER_ORGANIZATION_DATA_PATH), 'utf-8');
+  const userOrgs = JSON.parse(userOrgData);
+  const userOrgRepository = dataSource.getRepository(UserOrganization);
+
+  for (const userOrgData of userOrgs) {
+    const userOrg = userOrgRepository.create(userOrgData as DeepPartial<UserOrganization>);
+    userOrg.member = savedUsers[userOrgData.memberIndex];
+    userOrg.organization = savedOrgs[userOrgData.organizationIndex];
+
+    await userOrgRepository.save(userOrg);
   }
 };
 
@@ -65,7 +83,7 @@ const main = async () => {
 
   await dataSource.initialize();
 
-  await loadAndSaveProjectsData(dataSource, 'test/fixtures/projects.json');
+  await generateTestData(dataSource);
 };
 
 main().then(() => {
