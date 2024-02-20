26260976fbfbd20c35e2620bf3c6933258e89948
Wahyu Kurniawan <ayungavis@gmail.com>
2024-02-20 18:24:49 +0700

    ⚡️ feat: create check icon for checkbox component

---
+++ packages/frontend/src/components/shared/CustomIcon/CheckIcon.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { CustomIcon, CustomIconProps } from './CustomIcon';
+
+export const CheckIcon = (props: CustomIconProps) => {
+  return (
+    <CustomIcon
+      width="12"
+      height="12"
+      viewBox="0 0 12 12"
+      fill="none"
+      {...props}
+    >
+      <path
+        d="M1.5 7.5L4.64706 10L10.5 2"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </CustomIcon>
+  );
+};
--- packages/frontend/src/components/shared/CustomIcon/index.ts
+++ packages/frontend/src/components/shared/CustomIcon/index.ts
@@ -1,2 +1,3 @@
 export * from './PlusIcon';
 export * from './CustomIcon';
+export * from './CheckIcon';
