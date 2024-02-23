6cc4070e8b966d9774eaca68f433b0fce5ed5098
neeraj <neeraj.rtly@gmail.com>
2024-02-23 09:28:17 +0530

    Fix format of eth address to display

--- packages/frontend/src/utils/format.ts
+++ packages/frontend/src/utils/format.ts
@@ -1,6 +1,6 @@
 export const formatAddress = (address: string) => {
-  if (address.startsWith('0x') && address.length > 8) {
-    return address.slice(0, 4) + '..' + address.slice(-4);
+  if (address.startsWith('0x') && address.length > 10) {
+    return address.slice(0, 6) + '..' + address.slice(-4);
   }
 
   return address;
