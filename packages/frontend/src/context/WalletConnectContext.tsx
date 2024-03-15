c4ced267836cf68538488e4e2c698ffc5892f40f
neeraj <neeraj.rtly@gmail.com>
2024-03-15 12:25:57 +0530

    Add method to sign message

--- packages/frontend/src/context/WalletConnectContext.tsx
+++ packages/frontend/src/context/WalletConnectContext.tsx
@@ -10,17 +10,24 @@ import React, {
 
 import SignClient from '@walletconnect/sign-client'
 import { WalletConnectModal } from "@walletconnect/modal";
+import { SessionTypes } from "@walletconnect/types";
+import assert from 'assert';
+import * as encoding from "@walletconnect/encoding";
 
 
 // TODO: Get from env
 const PROJECT_ID = "c97365bf9f06d12a7488de36240b0ff4"
 
 interface ContextValue {
-  connect: ()=> Promise<void>;
+  connect: () => Promise<void>;
+  signIn: () => Promise<void>;
+  isSession: boolean
 }
 
 const walletConnectContext = createContext<ContextValue>({
-  connect: async()=> {}
+  connect: async () => { },
+  signIn: async () => { },
+  isSession: false
 })
 
 const web3Modal = new WalletConnectModal({
@@ -30,8 +37,12 @@ const web3Modal = new WalletConnectModal({
 
 export const WalletConnectProvider = ({ children }: { children: ReactNode }) => {
   const [signClient, setSignClient] = useState<SignClient>()
+  const [session, setSession] = useState<SessionTypes.Struct>()
+  const [isSession, setisSession] = useState(false)
+  // const [chains, setChains] = useState<string[]>([]);
+  const [accounts, setAccounts] = useState<string[]>([]);
 
-  const createClient = async ()=> {
+  const createClient = async () => {
 
     const signClient = await SignClient.init({
       projectId: PROJECT_ID,
@@ -53,35 +64,73 @@ export const WalletConnectProvider = ({ children }: { children: ReactNode }) => 
       throw Error("SignClient does not exist");
     }
 
-    const proposalNamespace = {
-      eip155: {
+    try {
+      const proposalNamespace = {
+        eip155: {
           methods: [
             'personal_sign',
           ],
-          chains: ['eip155:1', 'eip155:5'],
+          chains: ['eip155:1'],
           events: ["connect", "disconnect"]
         }
       }
 
-    const { uri, approval } = await signClient.connect({
-      requiredNamespaces: proposalNamespace
-    });
+      const { uri, approval } = await signClient.connect({
+        requiredNamespaces: proposalNamespace
+      });
 
-    if (uri){
-      web3Modal.openModal({ uri });
-      const session = await approval()
+      if (uri) {
+        web3Modal.openModal({ uri });
+        const currentSession = await approval()
+        setSession(currentSession)
+        setisSession(true)
+        const allNamespaceAccounts = Object.values(currentSession.namespaces)
+          .map((namespace) => namespace.accounts)
+          .flat();
+        const allNamespaceChains = Object.keys(currentSession.namespaces);
+        // setChains(allNamespaceChains);
+        setAccounts(allNamespaceAccounts);
+      }
+
+    } catch (err) {
+      console.log("Error in connecting", err)
+    } finally {
+      web3Modal.closeModal()
+    }
+  }
+
+  const signIn = async () => {
+    assert(signClient, "Sign client doesnot exist")
+    assert(session, "Wallet connect session not exist")
+
+    try {
+      const result = await signClient.request({
+        topic: session.topic,
+        chainId: 'eip155:1',
+        request: {
+          method: 'personal_sign',
+          params: [
+            encoding.utf8ToHex("sign message", true),
+            accounts[0].split(':')[2]
+          ]
+        }
+      })
+    } catch (error) {
+      console.log("err in signin ", error)
     }
   }
 
   useEffect(() => {
-    if(!signClient){
+    if (!signClient) {
       createClient();
     }
   }, [signClient])
 
   return (
     <walletConnectContext.Provider value={{
-    connect
+      connect,
+      signIn,
+      isSession
     }}>
       {children}
     </walletConnectContext.Provider>
@@ -90,10 +139,10 @@ export const WalletConnectProvider = ({ children }: { children: ReactNode }) => 
 
 export const useWalletConnectContext = () => {
   const {
-    connect
+    connect, signIn, isSession
   } = useContext(walletConnectContext);
 
   return {
-    connect
+    connect, signIn, isSession
   };
 };
--- packages/frontend/src/pages/Login.tsx
+++ packages/frontend/src/pages/Login.tsx
@@ -3,7 +3,7 @@ import './Snow.css';
 import { useWalletConnectContext } from 'context/WalletConnectContext';
 
 const Login = () => {
-  const { connect } = useWalletConnectContext()
+  const { connect, isSession, signIn } = useWalletConnectContext()
 
   return (
     <div className="flex items-center justify-center h-screen bg-snowball-900 snow">
@@ -14,7 +14,7 @@ const Login = () => {
           className="w-32 h-32 rounded-full mb-4"
         />
         {/* <w3m-button /> */}
-        <button onClick={connect}>Connect</button>
+        {isSession ? (<><button onClick={signIn}>Sign message</button></>):(<><button onClick={connect}>Connect</button></>) }
       </div>
     </div>
   );
