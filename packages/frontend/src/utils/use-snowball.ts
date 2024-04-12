baadc507e71e2c3b7d3db473c30eaad29b13e95b
Gilbert <gilbertbgarza@gmail.com>
2024-04-11 21:59:33 -0500

    Not yet

--- packages/frontend/src/utils/use-snowball.ts
+++ packages/frontend/src/utils/use-snowball.ts
@@ -1,13 +1,13 @@
-import { useEffect, useState } from 'react';
-import { snowball } from './snowball';
+// import { useEffect, useState } from 'react';
+// import { snowball } from './snowball';
 
-export function useSnowball() {
-  const [state, setState] = useState(100);
+// export function useSnowball() {
+//   const [state, setState] = useState(100);
 
-  useEffect(() => {
-    // Subscribe and directly return the unsubscribe function
-    return snowball.subscribe(() => setState(state + 1));
-  }, [snowball]);
+//   useEffect(() => {
+//     // Subscribe and directly return the unsubscribe function
+//     return snowball.subscribe(() => setState(state + 1));
+//   }, [snowball]);
 
-  return snowball;
-}
+//   return snowball;
+// }
