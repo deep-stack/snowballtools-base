d34a5f29cd0d55b21b38024419cfc697d5e507b5
Gilbert <gilbertbgarza@gmail.com>
2024-04-24 12:18:59 -0500

    Prettier format

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      utils: "/src/utils",
-      assets: "/src/assets",
-      context: "/src/context",
-      components: "/src/components",
+      utils: '/src/utils',
+      assets: '/src/assets',
+      context: '/src/context',
+      components: '/src/components',
     },
   },
   optimizeDeps: {
