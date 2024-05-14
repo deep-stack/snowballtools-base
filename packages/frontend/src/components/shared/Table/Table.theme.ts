4c3072ed50722d88a26788375fa089cc63e03bb4
Vivian Phung <dev+github@vivianphung.com>
2024-05-14 18:45:38 -0400

    [19/n][Storybook] basic Table uses themes

--- packages/frontend/src/components/shared/Table/Table.theme.ts
+++ packages/frontend/src/components/shared/Table/Table.theme.ts
@@ -1,35 +1,49 @@
-interface ThemeProps {
-  base: string;
-  header: string;
-  row: string;
-  columnHeaderCell: string;
-  rowHeaderCell: string;
-  cell: string;
-}
+import { tv, VariantProps } from 'tailwind-variants';
 
-const primaryTheme: ThemeProps = {
-  base: 'min-w-full border-collapse',
-  header: 'border-b border-sky-950/opacity-5 font-medium leading-tight',
-  row: 'border-b border-sky-950/opacity-5',
-  columnHeaderCell:
-    'p-4 text-sky-950 text-sm font-medium uppercase tracking-wider text-left',
-  rowHeaderCell:
-    'p-4 text-slate-600 text-sm font-normal leading-tight text-left',
-  cell: 'p-4 whitespace-nowrap text-sm text-slate-600 font-normal text-left',
-};
+export const tableTheme = tv({
+  slots: {
+    root: ['min-w-full', 'border-collapse'],
+    header: [
+      'p-2',
+      'border-b',
+      'border-sky-950/opacity-5',
+      'text-sky-950',
+      'text-sm',
+      'font-medium',
+      'leading-tight',
+    ],
+    body: [],
+    row: ['border-b', 'border-sky-950/opacity-5'],
+    columnHeaderCell: [
+      'p-4',
+      'text-sky-950',
+      'text-sm',
+      'font-medium',
+      'uppercase',
+      'tracking-wider',
+      'text-left',
+    ],
+    rowHeaderCell: [
+      'p-4',
+      'text-slate-600',
+      'text-sm',
+      'font-normal',
+      'leading-tight',
+      'text-left',
+    ],
+    cell: [
+      'p-4',
+      'whitespace-nowrap',
+      'text-sm',
+      'text-slate-600',
+      'font-normal',
+      'text-left',
+    ],
+  },
+  variants: {},
+  defaultVariants: {
+    orientation: 'vertical',
+  },
+});
 
-const secondaryTheme: ThemeProps = {
-  base: 'min-w-full border-collapse',
-  header:
-    'p-2 border-b border-sky-950/5 text-sky-950 text-sm font-medium leading-tight',
-  row: 'text-left border-b border-gray-300/50',
-  columnHeaderCell:
-    'px-6 py-3 text-xs font-medium uppercase tracking-wider text-gray-800',
-  rowHeaderCell: 'px-6 py-4 font-medium whitespace-nowrap text-gray-800',
-  cell: 'px-6 py-4 whitespace-nowrap text-sm text-gray-600',
-};
-
-export const tableTheme = {
-  primary: primaryTheme,
-  secondary: secondaryTheme,
-};
+export type TableTheme = VariantProps<typeof tableTheme>;
