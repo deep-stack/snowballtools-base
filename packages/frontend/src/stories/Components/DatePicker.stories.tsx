a63d0f69ed9ee78a90422fcdab5f81d90b1ad009
Vivian Phung <dev+github@vivianphung.com>
2024-05-14 15:48:01 -0400

    [7/n][Storybook] DatePicker update argTypes

--- packages/frontend/src/stories/Components/DatePicker.stories.tsx
+++ packages/frontend/src/stories/Components/DatePicker.stories.tsx
@@ -6,6 +6,23 @@ const meta: Meta<typeof DatePicker> = {
   title: 'Components/DatePicker',
   component: DatePicker,
   tags: ['autodocs'],
+  argTypes: {
+    calendarProps: {
+      control: 'object',
+    },
+    onChange: {
+      action: 'change',
+    },
+    value: {
+      control: 'text',
+    },
+    selectRange: {
+      control: 'boolean',
+    },
+    onReset: {
+      action: 'reset',
+    },
+  },
 };
 
 export default meta;
@@ -13,13 +30,21 @@ export default meta;
 type Story = StoryObj<typeof DatePicker>;
 
 export const Default: Story = {
-  render: ({ calendarProps, onChange, value, selectRange, onReset }) => (
+  render: ({
+    calendarProps,
+    onChange,
+    value,
+    selectRange,
+    onReset,
+    ...args
+  }) => (
     <DatePicker
       calendarProps={calendarProps}
       onChange={onChange}
       value={value}
       selectRange={selectRange}
       onReset={onReset}
+      {...args}
     />
   ),
 };
