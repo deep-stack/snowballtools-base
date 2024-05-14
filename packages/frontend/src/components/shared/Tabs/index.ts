7c8e9f24486fbf8c40d35bc724dafd1619092440
Vivian Phung <dev+github@vivianphung.com>
2024-05-14 18:53:50 -0400

    Tabs component export TabsTheme

--- packages/frontend/src/components/shared/Tabs/index.ts
+++ packages/frontend/src/components/shared/Tabs/index.ts
@@ -1,4 +1,5 @@
 export * from './Tabs';
+export * from './Tabs.theme';
 export { default as TabsList } from './TabsList';
 export { default as TabsTrigger } from './TabsTrigger';
 export { default as TabsContent } from './TabsContent';
