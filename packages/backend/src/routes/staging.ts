e2bf5d052c73f069083012434d1a142331c65354
Gilbert <gilbertbgarza@gmail.com>
2024-06-18 17:46:47 -0500

    Bump server version

--- packages/backend/src/routes/staging.ts
+++ packages/backend/src/routes/staging.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/version', async (req, res) => {
-  return res.send({ version: '0.0.2' });
+  return res.send({ version: '0.0.3' });
 });
 
 export default router;
