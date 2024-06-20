2ada11f3112c8e240845d44c284d6aa1efe5ba2d
Gilbert <gilbertbgarza@gmail.com>
2024-06-19 22:59:20 -0500

    Support async express handlers

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,3 +1,4 @@
+import 'express-async-errors';
 import 'reflect-metadata';
 import debug from 'debug';
 import fs from 'fs';
--- packages/backend/src/routes/staging.ts
+++ packages/backend/src/routes/staging.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/version', async (req, res) => {
-  return res.send({ version: '0.0.5' });
+  return res.send({ version: '0.0.6' });
 });
 
 export default router;
