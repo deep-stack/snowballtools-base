82a1c151a8a72cc7e09557852313433d51271273
Vivian Phung <dev+github@vivianphung.com>
2024-06-20 00:32:41 -0400

    Refactor: `AssignDomainDialog` uses `Modal` component (#202)
    ### TL;DR
    
    This PR includes updates to the project settings.
    
    ### What changed?
    
    The project settings have been refactored for better organization and readability.
    
    ### How to test?
    
    To test this change, navigate to the project settings and ensure all options are functioning as expected.
    
    ### Why make this change?
    
    This change was made to improve the user experience when navigating through the project settings.
    
    ---

--- packages/frontend/src/components/projects/project/deployments/AssignDomainDialog.tsx
+++ packages/frontend/src/components/projects/project/deployments/AssignDomainDialog.tsx
@@ -1,13 +1,8 @@
 import { CopyBlock, atomOneLight } from 'react-code-blocks';
 import { Link } from 'react-router-dom';
 
-import {
-  Button,
-  Dialog,
-  DialogHeader,
-  DialogBody,
-  DialogFooter,
-} from '@snowballtools/material-tailwind-react-fork';
+import { Modal } from 'components/shared/Modal';
+import { Button } from 'components/shared/Button';
 
 interface AssignDomainProps {
   open: boolean;
@@ -16,36 +11,31 @@ interface AssignDomainProps {
 
 const AssignDomainDialog = ({ open, handleOpen }: AssignDomainProps) => {
   return (
-    <Dialog open={open} handler={handleOpen}>
-      <DialogHeader>Assign Domain</DialogHeader>
-      <DialogBody>
-        In order to assign a domain to your production deployments, configure it
-        in the{' '}
-        {/* TODO: Fix selection of project settings tab on navigation to domains */}
-        <Link to="../settings/domains" className="text-light-blue-800 inline">
-          project settings{' '}
-        </Link>
-        (recommended). If you want to assign to this specific deployment,
-        however, you can do so using our command-line interface:
-        {/* https://github.com/rajinwonderland/react-code-blocks/issues/138 */}
-        <CopyBlock
-          text="snowball alias <deployment> <domain>"
-          language=""
-          showLineNumbers={false}
-          theme={atomOneLight}
-        />
-      </DialogBody>
-      <DialogFooter className="flex justify-start">
-        <Button
-          className="rounded-3xl"
-          variant="gradient"
-          color="blue"
-          onClick={handleOpen}
-        >
-          <span>Okay</span>
-        </Button>
-      </DialogFooter>
-    </Dialog>
+    <Modal open={open} onOpenChange={handleOpen}>
+      <Modal.Content>
+        <Modal.Header>Assign Domain</Modal.Header>
+        <Modal.Body>
+          In order to assign a domain to your production deployments, configure
+          it in the{' '}
+          {/* TODO: Fix selection of project settings tab on navigation to domains */}
+          <Link to="../settings/domains" className="text-light-blue-800 inline">
+            project settings{' '}
+          </Link>
+          (recommended). If you want to assign to this specific deployment,
+          however, you can do so using our command-line interface:
+          {/* https://github.com/rajinwonderland/react-code-blocks/issues/138 */}
+          <CopyBlock
+            text="snowball alias <deployment> <domain>"
+            language=""
+            showLineNumbers={false}
+            theme={atomOneLight}
+          />
+        </Modal.Body>
+        <Modal.Footer className="flex justify-start">
+          <Button onClick={handleOpen}>Okay</Button>
+        </Modal.Footer>
+      </Modal.Content>
+    </Modal>
   );
 };
 
