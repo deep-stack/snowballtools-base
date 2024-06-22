ce1833cb51c2af00d4442737f29af18fcc7e3b74
Vivian Phung <dev+github@vivianphung.com>
2024-06-21 21:16:34 -0400

    bump: version (#213)
    This pull request updates the backend version endpoint to return version '0.0.8' instead of '0.0.7'.
    
    ---

--- packages/backend/src/routes/staging.ts
+++ packages/backend/src/routes/staging.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/version', async (req, res) => {
-  return res.send({ version: '0.0.7' });
+  return res.send({ version: '0.0.8' });
 });
 
 export default router;
