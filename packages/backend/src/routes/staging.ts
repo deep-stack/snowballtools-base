94f46f9621635471e6091c9ba2db87fd0436ed59
Vivian Phung <dev+github@vivianphung.com>
2024-06-24 19:52:51 -0400

    v.0.0.9 staging (#229)

--- packages/backend/src/routes/staging.ts
+++ packages/backend/src/routes/staging.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/version', async (req, res) => {
-  return res.send({ version: '0.0.8' });
+  return res.send({ version: '0.0.9' });
 });
 
 export default router;
