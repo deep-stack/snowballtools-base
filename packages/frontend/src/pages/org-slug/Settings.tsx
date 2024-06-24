1b038476c7f529ec1acfafa1097ecca13668b347
Vivian Phung <dev+github@vivianphung.com>
2024-06-24 18:57:38 -0400

    feat: `Settings` screen for org (#224)
    This update introduces a new header section within the `Settings` page. It incorporates a `Heading` component from the shared components library, ensuring consistent styling and structure across the app. The `Settings` page layout now features appropriate padding and flexbox for better alignment and spacing.
    
    ---

--- packages/frontend/src/pages/org-slug/Settings.tsx
+++ packages/frontend/src/pages/org-slug/Settings.tsx
@@ -1,5 +1,19 @@
-const Settings = () => {
-  return <div className="p-5">Settings page</div>;
-};
+import { Heading } from 'components/shared/Heading';
 
+const Settings = () => {
+  return (
+    <section className="px-4 md:px-6 py-6 flex flex-col gap-6">
+      {/* Header */}
+      <div className="flex items-center">
+        <div className="grow">
+          <div className="flex gap-4 items-center">
+            <Heading as="h2" className="text-[24px]">
+              Settings
+            </Heading>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 export default Settings;
