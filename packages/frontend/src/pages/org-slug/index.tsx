9f0a2ad548385ce1e6045c7edd9cdf8aec2d0dca
Vivian Phung <dev+github@vivianphung.com>
2024-07-03 12:41:39 -0400

    space

--- packages/frontend/src/pages/org-slug/index.tsx
+++ packages/frontend/src/pages/org-slug/index.tsx
@@ -26,6 +26,7 @@ const Projects = () => {
   return (
     <section className="px-4 md:px-6 py-6 flex flex-col gap-6">
       {/* Header */}
+
       <div className="flex items-center">
         <div className="grow">
           <div className="flex gap-4 items-center">
