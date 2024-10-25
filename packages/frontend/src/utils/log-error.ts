b449c299dc3e6f1bbdad2a89e563b94b2e8bb88e
Nabarun <nabarun@deepstacksoft.com>
2024-10-25 12:40:34 +0000

    Comment out bugsnag code (#16)
    Part of [Service provider auctions for web deployments](https://www.notion.so/Service-provider-auctions-for-web-deployments-104a6b22d47280dbad51d28aa3a91d75)
    
    Reviewed-on: https://git.vdb.to/cerc-io/snowballtools-base/pulls/16
    Co-authored-by: Nabarun <nabarun@deepstacksoft.com>
    Co-committed-by: Nabarun <nabarun@deepstacksoft.com>

--- packages/frontend/src/utils/log-error.ts
+++ packages/frontend/src/utils/log-error.ts
@@ -1,34 +1,36 @@
-import React from 'react';
-import Bugsnag from '@bugsnag/js';
-import BugsnagPluginReact from '@bugsnag/plugin-react';
-import BugsnagPerformance from '@bugsnag/browser-performance';
+// import React from 'react';
+// import Bugsnag from '@bugsnag/js';
+// import BugsnagPluginReact from '@bugsnag/plugin-react';
+// import BugsnagPerformance from '@bugsnag/browser-performance';
 
-import { VITE_BUGSNAG_API_KEY } from './constants';
+// import { VITE_BUGSNAG_API_KEY } from './constants';
 
-if (VITE_BUGSNAG_API_KEY) {
-  Bugsnag.start({
-    apiKey: VITE_BUGSNAG_API_KEY,
-    plugins: [new BugsnagPluginReact()],
-  });
-  BugsnagPerformance.start({ apiKey: VITE_BUGSNAG_API_KEY });
-}
+// if (VITE_BUGSNAG_API_KEY) {
+//   Bugsnag.start({
+//     apiKey: VITE_BUGSNAG_API_KEY,
+//     plugins: [new BugsnagPluginReact()],
+//   });
+//   BugsnagPerformance.start({ apiKey: VITE_BUGSNAG_API_KEY });
+// }
 
-export const errorLoggingEnabled = !!VITE_BUGSNAG_API_KEY;
+// export const errorLoggingEnabled = !!VITE_BUGSNAG_API_KEY;
 
-export const LogErrorBoundary = VITE_BUGSNAG_API_KEY
-  ? Bugsnag.getPlugin('react')!.createErrorBoundary(React)
-  : ({ children }: any) => children;
+// export const LogErrorBoundary = VITE_BUGSNAG_API_KEY
+//   ? Bugsnag.getPlugin('react')!.createErrorBoundary(React)
+//   : ({ children }: any) => children;
 
-export function logError(error: Error) {
-  let errors: any[] = [error];
-  let safety = 0;
-  while (errors[errors.length - 1].cause && safety < 10) {
-    errors.push('::caused by::', errors[errors.length - 1].cause);
-    safety += 1;
-  }
-  console.error(...errors);
+// export function logError(error: Error) {
+//   let errors: any[] = [error];
+//   let safety = 0;
+//   while (errors[errors.length - 1].cause && safety < 10) {
+//     errors.push('::caused by::', errors[errors.length - 1].cause);
+//     safety += 1;
+//   }
+//   console.error(...errors);
 
-  if (VITE_BUGSNAG_API_KEY) {
-    Bugsnag.notify(error);
-  }
-}
+//   if (VITE_BUGSNAG_API_KEY) {
+//     Bugsnag.notify(error);
+//   }
+// }
+
+export const LogErrorBoundary = ({ children }: any) => children;
